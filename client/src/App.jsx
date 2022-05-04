81743ef989818021765a5a872dc4281fb3774aee
Divy Parekh <divyparekh1810@gmail.com>
2022-05-04 19:16:12 +0530

    Folder Structure is Done

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat';
+import { ChannelList, Chat } from 'stream-chat-react';
+
+import Cookies from 'universal-cookie';
+
+import {ChannelContainer, ChannelListContainer} from './Components';
+const apiKey = 'ya4t2da49psp';
+const client = StreamChat.getInstance(apiKey);
+const App = () => {
+  return (
+    <div className='app__wrapper'>
+        <Chat client={client} theme="team light">
+           <ChannelListContainer 
+            
+           />
+           <ChannelContainer 
+           
+           />
+        </Chat>
+    </div>
+  )
+}
+
+export default App;

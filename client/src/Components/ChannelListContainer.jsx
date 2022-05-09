dd338f143dafbeee7b0246d280efc60204e0b5c0
Divy Parekh <divyparekh1810@gmail.com>
2022-05-09 19:34:25 +0530

    initial structure is Done and starting with the client side auth

--- client/src/Components/ChannelListContainer.jsx
+++ client/src/Components/ChannelListContainer.jsx
@@ -1,57 +1,57 @@
-import React from 'react'
-import { ChannelList, useChatContext } from 'stream-chat-react';
-import Cookies from 'universal-cookie';
-import HospitalIcon from '../assets/hospital.png'
-import LogoutIcon from '../assets/logout.png'
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
 
-import { ChannelSearch,TeamChannelList, TeamChannelPreview } from './';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 
 const SideBar = () => (
-  <div className='channel-list__sidebar'>
-      <div className='channel-list__sidebar__icon1'>
-        <div className='icon1__inner'>
-          <img src={HospitalIcon} alt="Hospital" width="30"/>
-        </div>
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={HospitalIcon} alt="Hospital" width="30" />
       </div>
-      <div className='channel-list__sidebar__icon2'>
-        <div className='icon1__inner'>
-          <img src={LogoutIcon} alt="LogoutIcon" width="30"/>
-        </div>
+    </div>
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="LogoutIcon" width="30" />
       </div>
+    </div>
   </div>
-)
+);
 
 const CompanyHeader = () => (
-  <div className='channel-list__header'>
-    <p className='channel-list__header__text'>HeyDoc</p>
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">HeyDoc</p>
   </div>
-)
+);
 const ChannelListContainer = () => {
   return (
     <>
       <SideBar />
-      <div className='channel-list__list__wrapper'>
+      <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch />
-        <ChannelList 
-        filters={{}}
-        channelRenderFilterFn={()=>{}}
-        List={(listProps) => (
-          <TeamChannelList 
-          {...listProps}
-          type="team"
-          />
-        )}
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
           Preview={(previewProps) => (
-            <TeamChannelPreview 
-            {...previewProps}
-            type="team"
-            />
+            <TeamChannelPreview {...previewProps} type="team" />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="messaging" />}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="messaging" />
           )}
         />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default ChannelListContainer;
